407ca911c6637f963b66e9f7ab637d4fb23c3b2c
Seiji Takeda <takeda@archetyp.jp>
2017-01-27 12:09:33 +0900

    “index.html”

---
+++ script.jsx
@@ -0,0 +1,26 @@
+var Oracle = React.createClass({
+    getInitialState:function(){
+        return { result:"Reactおみくじ"};
+    },
+
+    show: function(){ 
+        var arr = ['大吉', '小吉','中吉',"吉",'凶',"大凶"];
+        var index = Math.floor(Math.random()*arr.length);
+        this.setState({result: arr[index]});
+    },
+
+
+    render: function(){
+        return (
+            <div>
+                <p>{this.state.result}</p>
+                <button onClick={this.show}>Push</button>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Oracle　/>,
+    document.getElementById('root')
+);
